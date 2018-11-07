c6b37603ec05c3cb942ba40f63d645148db62f7f
Saudi Shahid <bilalshahid@macbook-pro-3.local>
2018-11-07 11:22:26 +0500

    Initial Commit

---
+++ Menu_FYP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Menu_FYP
+//
+//  Created by Bilal Shahid on 11/7/18.
+//  Copyright © 2018 Bilal Shahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Menu_FYPUITests/Menu_FYPUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Menu_FYPUITests.swift
+//  Menu_FYPUITests
+//
+//  Created by Bilal Shahid on 11/7/18.
+//  Copyright © 2018 Bilal Shahid. All rights reserved.
+//
+
+import XCTest
+
+class Menu_FYPUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
