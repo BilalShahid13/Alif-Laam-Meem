cfcae82bae7ecad4607839930d2c00b1c72c64d1
BilalShahid13 <bilalscheema@gmail.com>
2019-06-10 22:26:21 +0500

    FYP

---
+++ Menu_FYP/StudentView/Quran/QuranCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  QuranCollectionViewCell.swift
+//  Alif_Laam_Meem
+//
+//  Created by Bilal Shahid on 2/13/19.
+//  Copyright © 2019 Bilal Shahid. All rights reserved.
+//
+
+import UIKit
+
+class QuranCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var pageText: UIImageView!
+    @IBOutlet weak var pageLabel: UILabel!
+    
+}
